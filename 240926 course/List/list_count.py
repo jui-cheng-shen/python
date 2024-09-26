93021a857f8b98063a428023e4e95d27cb2b5b4b
jui-cheng-shen <150934759+jui-cheng-shen@users.noreply.github.com>
2024-09-26 11:49:53 +0800

    Add files via upload

---
+++ 240926 course/List/list_count.py
@@ -0,0 +1,4 @@
+a = ['apple', 'banana', 'banana', 'orange']  # 定義一個包含四個字串元素的列表 a
+# 使用 count 方法計算 'banana' 在列表 a 中出現的次數，並輸出結果
+print(a.count('banana'));
+print(a.count('orange'));
---
+++ 240926 course/List/list_join.py
@@ -0,0 +1,2 @@
+a = ['apple', 'banana', 'banana', 'orange']
+print(', '.join(a))  # 使用 join 方法將列表 a 中的元素連接成一個新的字串，並輸出結果
