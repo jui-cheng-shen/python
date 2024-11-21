91c9719f48f70d91577b4ce7053fc930c23e0828
jui-cheng-shen <150934759+jui-cheng-shen@users.noreply.github.com>
2024-11-21 11:22:43 +0800

    Add files via upload

---
+++ 241121 course/random/random_normalvariate.py
@@ -0,0 +1,7 @@
+import random;
+#取得平均數 100，標準為 10,得到的資料分布在90~110之間
+data=random.normalvariate(100,10)
+print(data);
+#取得平均數 100，標準為5,得到的資料分布在95~105之間
+data=random.normalvariate(100,5);
+print(data);
